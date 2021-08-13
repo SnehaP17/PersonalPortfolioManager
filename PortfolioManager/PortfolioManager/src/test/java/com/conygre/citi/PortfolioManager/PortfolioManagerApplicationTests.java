be0b46f0df0cf307cd0138536c87674ba066e2b0
unknown <zmorsi@ufl.edu>
2021-08-13 14:00:26 +0000

    initial

---
+++ PortfolioManager/PortfolioManager/src/test/java/com/conygre/citi/PortfolioManager/PortfolioManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conygre.citi.PortfolioManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
